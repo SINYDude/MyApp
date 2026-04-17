d7dd869dcdf81f7a779f443b5d2d22f2475a27cb
SINYDude <46901910+SINYDude@users.noreply.github.com>
2026-04-17 18:24:02 -0500

    second

--- obj/Debug/net10.0/MyApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46eac6c52aee51075ba46f4ca4177bb14939b985")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
